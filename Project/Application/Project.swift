a97e1b183f62c445c8fea6ad69cc0871c81db643
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2025-03-20 21:07:21 +0900

    feat: add Modules

--- Project/Application/Project.swift
+++ Project/Application/Project.swift
@@ -65,6 +65,11 @@ extension [String: Plist.Value] {
 extension [TargetDependency] {
   static var compositeValue: [TargetDependency] {
     [
+      .package(product: "Architecture", type: .runtime, condition: .none),
+      .package(product: "Domain", type: .runtime, condition: .none),
+      .package(product: "Platform", type: .runtime, condition: .none),
+      .package(product: "Functor", type: .runtime, condition: .none),
+      .package(product: "DesignSystem", type: .runtime, condition: .none),
     ]
   }
 }
@@ -72,6 +77,11 @@ extension [TargetDependency] {
 extension [Package] {
   static var compositeValue: [Package] {
     [
+      .local(path: .relativeToRoot("Modules/Core/Architecture")),
+      .local(path: .relativeToRoot("Modules/Core/Platform")),
+      .local(path: .relativeToRoot("Modules/Core/Domain")),
+      .local(path: .relativeToRoot("Modules/Core/Functor")),
+      .local(path: .relativeToRoot("Modules/Core/DesignSystem")),
     ]
   }
 }
